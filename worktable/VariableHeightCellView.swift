8ba37792eae756efff4d460061af19395fac0cb1
Maic Lopez Saenz <maic@lopsae.com>
2015-08-28 16:52:46 -0700

    add extra notes about cells with changing height

--- worktable/VariableHeightCellView.swift
+++ worktable/VariableHeightCellView.swift
@@ -34,6 +34,22 @@ class VariableHeightCellView: UITableViewCell, WorktableCellView {
 	}
 
 
+	/**
+	* Upon initialization the cell frame will be set to either its default size
+	* or the size provided by the nib file.
+	* 
+	* At some point table.configureCellForDisplay is called and with it an
+	* updated frame is provided. If autolayout is being used this updated frame
+	* will already consider the size of autolayed ui components.
+	*
+	* Later table.updateVisibleCell is called which provides an updated frame
+	* for any cells that provide a custom height through the table delegage.
+	*
+	* Afterwards layoutSubviews is called. If size is changed during this method
+	* it is necesary for the table to perform a beginUpdates-endUpdates and that
+	* the cell provides the new correct height for it to be updated with an
+	* animation.
+	*/
 	override func layoutSubviews() {
 		var newBounds = bounds
 		newBounds.size.height = definedHeight * 2
