851f46618874310718f9d997e6364e5a871f892f
Maic Lopez Saenz <maic@lopsae.com>
2015-09-10 12:09:14 -0700

    add documentation to the array extension

--- worktable/ArrayExtension.swift
+++ worktable/ArrayExtension.swift
@@ -2,6 +2,10 @@
 
 extension Array {
 
+	/**
+	Overwrites the first element of the array with the `newFirst` element. If
+	the array is empty the `newFirst` element is appended.
+	*/
 	mutating func setFirst(newFirst: T) {
 		if isEmpty {
 			append(newFirst)
@@ -11,6 +15,10 @@ extension Array {
 	}
 
 
+	/**
+	Overwrites the last element of the array with the `newLast` element. If
+	the array is empty the `newFirst` element is appended.
+	*/
 	mutating func setLast(newLast: T) {
 		if isEmpty {
 			append(newLast)
@@ -20,6 +28,21 @@ extension Array {
 	}
 
 
+	/**
+	Subscript that fills the array up to a given position and then returns or
+	overwrite the content of that position.
+	
+	For both get and set — if the array count is lower that `index` the array is
+	filled up with the `filler` element up the `index` position.
+	
+	When getting — the subscript will return either the element that was already
+	at the `index` position, or will fill up to the `index` position and then
+	return the `filler` element.
+	
+	When setting — the subscript will overwrite the element that was already at
+	the `index` position, or will fill up to the `index` position and then set
+	the last element with the `newValue` element.
+	*/
 	subscript(index: Int, #filler: T) -> T {
 		mutating get {
 			fill(to: index, filler: filler)
@@ -33,6 +56,12 @@ extension Array {
 	}
 
 
+	/**
+	Fills the array to the `index` position with the `filler` element.
+	
+	If the array count is already greater that `index` no operation is
+	performed.
+	*/
 	mutating func fill(to index: Int, filler: T) {
 		while count <= index {
 			append(filler)
