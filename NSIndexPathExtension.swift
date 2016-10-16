62e7c17bb69216192a18d052666d1971f894c4f9
Maic Lopez Saenz <maic@lopsae.com>
2016-10-15 20:05:25 -0700

    remove unnecesary methods from indexpath

--- NSIndexPathExtension.swift
+++ NSIndexPathExtension.swift
@@ -3,20 +3,6 @@ import Foundation
 
 extension IndexPath {
 
-	static func withIndexes(_ indexes: [Int]) -> IndexPath {
-		var indexPath = IndexPath()
-		for index in indexes {
-			indexPath = indexPath.appending(index)
-		}
-		return indexPath
-	}
-
-
-	static func withIndexes(_ indexes: Int...) -> IndexPath {
-		return self.withIndexes(indexes)
-	}
-
-
 	func toArray() -> [Int] {
 		var indexes = [Int](repeating: 0, count: self.count)
 		for index in 0 ..< self.count {
