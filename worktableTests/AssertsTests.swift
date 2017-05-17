abf44b39a24beb22a42e27e8183d792306c71bab
Maic Lopez Saenz <maic@lopsae.com>
2017-05-16 18:32:41 -0700

    allow optional into `equatable.assert(equals:)`
    and added tests for all combinations of equatables and optionals

--- worktableTests/AssertsTests.swift
+++ worktableTests/AssertsTests.swift
@@ -7,30 +7,49 @@ class AssertsTests: XCTestCase {
 	func testEquatableEquals() {
 		// equatable equals equatable
 		let string = "test"
-		string.assert(equal: "test")
+		string.assert(equals: "test")
 
 		// equatable equals optional
-
-		// equatable equals optional
+		let maybe: String? = "test"
+		string.assert(equals: maybe)
 
 		// equatable equals nil
-
-		// optional equals equatable
-
-		// optional equals optional
-
-		// optional equals nil
-
-		// nil equals equatable
-
-		// nil equals optional
-
-		// nil equals nil
+		// invalid, would never be equal
 	}
+
 
 	func testOptionalEquals() {
-		
+		// optional equals equatable
+		var maybe: String? = "test"
+		maybe.assert(equals: "test")
+
+		// optional equals optional
+		maybe.assert(equals: maybe)
+
+		// optional-nil equals optional nil
+		maybe = nil
+		maybe.assert(equals: maybe)
+
+		// optional-nil equals literal-nil
+		maybe.assert(equals: nil)
 	}
-	
+
+
+	func testImpossibleEquals() {
+		// Empty, to remember other impossible cases
+
+		// optional-nil equals equatable
+		// invalid, would never be equal
+
+		// literal-nil equals optional
+		// not valid, cannot infer type from `nil`
+		// nil.assert(equals: maybe)
+
+		// literal-nil equals literal-nil
+		// invalid, cannot infer type from `nil`
+		// nil.assert(equals: nil)
+	}
+
+
 }
 
--- worktableTests/EquatableTestsExtensions.swift
+++ worktableTests/EquatableTestsExtensions.swift
@@ -5,14 +5,14 @@ extension Equatable {
 
 	// TODO document reminder of why assert(equals:) needs to exists both here and in optional
 	func assert<T>(
-		equals expected: T,
+		equals expected: T?,
 		_ message: @autoclosure () -> String = String(),
 		file: StaticString = #file,
 		line: UInt = #line
 	) where T: Equatable {
 		guard let casted = expected as? Self
 		else {
-			let guardMessage = "equatable.assert(equal:) failed: Cannot cast parameter \"\(expected)\" into Self"
+			let guardMessage = "equatable.assert(equal:) failed: Cannot cast parameter \"\(expected.debugDescription)\" into Self"
 			XCTFail("\(guardMessage) - \(message())", file: file, line: line)
 			return
 		}
